35b95cfe64c73add3e5706905122033c7bb05cc0
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-12-08 14:09:56 +0700

    style: change styling

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -11,15 +11,15 @@ export default function HeroSection() {
         src={heroBackground}
         alt="Background"
         fill
-        className="object-cover z-0"
+        className="object-cover z-0 max-h-screen"
         quality={100}
       />
-      <div className="flex flex-col z-10 mx-[25px] tablet:mx-[50px] laptop:mx-[70px] w-[80%] laptop:w-[50%] h-screen justify-end laptop:justify-center pb-[75px] tablet:pb-[150px] laptop:pb-0 ">
+      <div className="flex flex-col z-10 mx-[25px] tablet:mx-[50px] laptop:mx-[70px] desktop:mx-[150px] w-[80%] laptop:w-[50%] h-screen justify-end laptop:justify-center pb-[75px] tablet:pb-[150px] laptop:pb-0 ">
         <div className="flex flex-col gap-[20px] tablet:gap-[40px] self-center">
           <h1 className="text-white font-urbanist text-[28px] tablet:text-[50px] desktop:text-[64px] font-bold">
             Jasa buat website yang cepat dan aman
           </h1>
-          <p className="text-white font-poppins font-normal text-[10px] desktop:text-[20px] tablet:text-[16px]">
+          <p className="text-white font-poppins font-normal text-[12px] desktop:text-[20px] tablet:text-[16px]">
             Optimalkan performa bisnis anda melalui website berstandar tinggi.
             Dengan fokus pada kecepatan, SEO, dan responsivitas, kami wujudkan
             platform digital yang mendorong pertumbuhan bisnis Anda.
